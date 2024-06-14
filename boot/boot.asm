4cf327b049c70739eeb25b229c147a7db71203b0
Mark Watson <markus.c.watson@gmail.com>
2024-06-14 15:22:17 -0300

    setup data segments

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,7 +1,17 @@
-ORG 0x7c00
+ORG 0
 BITS 16
 
+jmp 0x7c00:start
+
 start:
+    cli
+    mov ax, 0x7c0       ; setup data segment
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+    sti
     mov si, message
     call print
     jmp $
@@ -9,7 +19,7 @@ start:
 print:
     mov bx, 0
 .loop:
-    lodsb  ; si -> al
+    lodsb               ; si -> al
     cmp al, 0
     je .done
     call print_char
@@ -18,8 +28,8 @@ print:
     ret
 
 print_char:
-    mov ah, 0eh ; print to screen in bios
-    int 0x10 ; invoke bios
+    mov ah, 0eh         ; print to screen in bios
+    int 0x10            ; invoke bios
     ret
 
 message: db 'Hello world!', 0
