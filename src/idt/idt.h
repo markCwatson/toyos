6fd8277bfd3caa06c345d7012923f63c2842557d
Mark Watson <markus.c.watson@gmail.com>
2024-07-06 21:19:24 -0300

    remap pic

--- src/idt/idt.h
+++ src/idt/idt.h
@@ -10,17 +10,17 @@ typedef void (*interrupt_cb_fn)();
 
 struct idt_desc
 {
-    uint16_t offset_1; // Offset bits 0 - 15
-    uint16_t selector; // Selector thats in our GDT
-    uint8_t zero; // Does nothing, unused set to zero
-    uint8_t type_attr; // Descriptor type and attributes
-    uint16_t offset_2; // Offset bits 16-31
+    uint16_t offset_1;      // Offset bits 0 - 15
+    uint16_t selector;      // Selector thats in our GDT
+    uint8_t zero;           // Does nothing, unused set to zero
+    uint8_t type_attr;      // Descriptor type and attributes
+    uint16_t offset_2;      // Offset bits 16-31
 } __attribute__((packed));
 
 struct idtr_desc
 {
-    uint16_t limit; // Size of descriptor table -1
-    uint32_t base; // Base address of the start of the interrupt descriptor table
+    uint16_t limit;         // Size of descriptor table -1
+    uint32_t base;          // Base address of the start of the interrupt descriptor table
 } __attribute__((packed));
 
 struct interrupt_frame
--- src/kernel.asm
+++ src/kernel.asm
@@ -24,6 +24,15 @@ _start:
     or al, 2
     out 0x92, al
 
+    ; remap master PIC
+    mov al, 00010001b
+    out 0x20, al
+    mov al, 0x20 ; ISR start at 0x20
+    out 0x21, al
+    mov al, 00000001b
+    out 0x21, al
+
+    ; call kernel main
     call kernel_main
     jmp $
 
